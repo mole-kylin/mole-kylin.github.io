b091b0520178c21cb16613cd36364339c2b83b89
mole-kylin <710558250@qq.com>
2020-11-15 15:54:27 +0800

    wu xie

---
+++ 未命名1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,result;
+	for(i=1;i<=9;i++)
+	{
+		for(j=1;j<=9;j++)
+		{
+			result=i*j;
+			printf("%d*%d=%-3d",i,j,result);
+		 } 
+		 printf("\n");
+	 }
+		return 0;
+ } 
